25590d533d6b89e010c6a66d23e8735b66ec4833
jessica sossou <merveille20sossou@gmail.com>
2021-08-30 22:11:12 +0100

    creat tweet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,31 @@
 class PostsController < ApplicationController
   def index
   end
+
   def list
     @posts = Post.all
   end
+
   def new
-    
+    @post =  Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:Retour]
+      render :new
+     else
+       if @post.save
+         flash[:notice] = " Eleve ajoute avec succes" 
+         redirect_to posts_path
+       else
+         render new_post_path
+       end
+     end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module TweeterClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Benin'
-c   onfig.active_record.default_timezone = :local
+    #config.time_zone = 'Benin'
+   # config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
